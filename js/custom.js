ae6445051a06ac56293df5b7d9a306d40ea15efb
Vincent Rondeux <vincent.r@mw-a.net>
2019-06-08 22:46:49 +0200

    Remove trailing whitespace + remove windows CRLF + adapt copy.

--- js/custom.js
+++ js/custom.js
@@ -86,7 +86,7 @@ $(document).ready(function() {
   /*  smoothscroll
   ----------------------------------------------*/
    $(function() {
-        $('#home a, .navbar-default a').bind('click', function(event) {
+        $('#home a, .navbar-default a, #feature a').bind('click', function(event) {
             var $anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top - 49
